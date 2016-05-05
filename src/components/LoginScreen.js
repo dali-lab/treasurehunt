f8519b8bb62f955868128779cbb18356a2acd70c
johnlev <john.lyme@mac.com>
2016-05-04 21:02:58 -0400

    Created prototype login screen

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -2,8 +2,6 @@
  
 var React = require('react-native');
 
-var HomePage = require('./HomePage');
-
 var {
   StyleSheet,
   Text,
@@ -16,60 +14,106 @@ var {
 } = React;
 
 var styles = StyleSheet.create({
-  description: {
-    marginBottom: 20,
-    fontSize: 18,
-    textAlign: 'center',
-    color: '#656565'
-  },
-  container: {
-    padding: 30,
-    marginTop: 65,
-    alignItems: 'center'
-  },
-  buttonText: {
-  fontSize: 18,
-  color: 'white',
-  alignSelf: 'center'
+	description: {
+		marginBottom: 40,
+		width: 90,
+		height: 90,
 	},
-  button: {
-	height: 36,
-	flex: 1,
-	flexDirection: 'row',
-	backgroundColor: '#48BBEC',
-	borderColor: '#48BBEC',
-	borderWidth: 1,
-	borderRadius: 8,
-	marginBottom: 10,
-	alignSelf: 'stretch',
-	justifyContent: 'center'
-  },
+	container: {
+		padding: 30,
+		marginTop: 50,
+		alignItems: 'center'
+	},
+	buttonText: {
+		fontSize: 18,
+		color: 'white',
+		alignSelf: 'center'
+	},
+	button: {
+		height: 36,
+		flex: 1,
+		flexDirection: 'row',
+		backgroundColor: '#48BBEC',
+		borderColor: '#48BBEC',
+		borderWidth: 1,
+		borderRadius: 8,
+		marginBottom: 10,
+		alignSelf: 'stretch',
+		justifyContent: 'center'
+	},
+	textField: {
+		height: 40,
+		flex: 1,
+		flexDirection: 'row',
+		fontSize: 20,
+		paddingLeft: 20,
+		paddingRight: 20,
+	},
+	loginTextInputStyle: {
+		height: 45,
+		borderColor: 'gray',
+		borderWidth: 3,
+		flex: 1,
+		flexDirection: 'row',
+		alignSelf: 'stretch',
+		bottom: 20,
+	},
+	loginIcons: {
+		top: 5,
+		left: 5,
+		height: 30,
+		width: 30,
+		alignSelf: 'stretch',
+		flexDirection: 'row',
+	},
 });
 
 class LoginScreen extends Component {
+	propTypes: {
+		onLogin: React.PropTypes.func,
+    }
+
 	constructor(props) {
 	  super(props);
 	}
+
 	onLoginPressed() {
-	  this.props.navigator.push({
-		  title: 'Treasure Hunt',
-		  component: HomePage,
-	  });
+		if (typeof this.props.onLogin == 'function') {
+			this.props.onLogin();
+		}
 	}
+
 	render() {
-		    return (
-		      <View style={styles.container}>
-		        <Text style={styles.description}>
-		          Login page for Treasure Hunt!
-		        </Text>
-		         <TouchableHighlight style={styles.button}
-		         	onPress={this.onLoginPressed.bind(this)}
-		      		underlayColor='#99d9f4'>
+		return (
+			<View style={styles.container}>
+				<Image
+					style={styles.description}
+					source={require('../../loginLogo.png')}/>
+
+				<View style={styles.loginTextInputStyle}>
+					<Image
+						style={styles.loginIcons}
+						source={require('../../user.png')}/>
+					<TextInput style= {styles.textField}
+						placeholder="Username"/>
+				</View>
+				<View style={styles.loginTextInputStyle}>
+					<Image
+						style={styles.loginIcons}
+						source={require('../../password.png')}/>
+					<TextInput style={styles.textField}
+						secureTextEntry={true}
+						placeholder="Password"/>
+				</View>
+
+				<TouchableHighlight style={styles.button}
+					onPress={this.onLoginPressed.bind(this)}
+					underlayColor='#99d9f4'>
 		    		<Text style={styles.buttonText}>Login</Text>
 		  		</TouchableHighlight>
-		      </View>
-		    );
-		  }
-		}
+		  	</View>
+	  	);
+	}
+}
 
 module.exports = LoginScreen;
