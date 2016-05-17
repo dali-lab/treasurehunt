2a67aff298512956534ce437d6fe59c0540b5991
johnlev <john.lyme@mac.com>
2016-05-17 15:20:49 -0400

    Almost done with login view!

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -10,23 +10,39 @@ var {
   TouchableHighlight,
   ActivityIndicatorIOS,
   Image,
-  Component
+  Component,
+  Dimensions,
+  Link
 } = React;
 
+var screenPadding = 60;
+var screenWidth = Dimensions.get('window').width;
+
 var styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		alignItems: "center",
+		flexDirection: "column",
+	},
+	innerContainer: {
+		flexDirection: "column",
+		flex: 1,
+		width: screenWidth * 2 / 3,
+		marginTop: screenPadding,
+		marginLeft: screenPadding,
+		marginRight: screenPadding,
+	},
 	icon: {
-		marginTop: 50,
 		width: 144,
 		height: 120,
-	},
-	container: {
-		alignItems: 'center'
+		alignSelf: "center"
 	},
 	titleStyle: {
 		marginTop: 20,
 		marginBottom: 40,
 		fontSize: 25,
 		color: "#59aa91",
+		alignSelf: "center"
 	},
 	buttonText: {
 		fontSize: 18,
@@ -36,14 +52,11 @@ var styles = StyleSheet.create({
 	},
 	button: {
 		height: 36,
-		flex: 1,
-		marginLeft: 50,
-		marginRight: 50,
 		flexDirection: 'row',
 		backgroundColor: '#bfcf60',
 		borderColor: '#bfcf60',
 		borderWidth: 1,
-		borderRadius: 8,
+		borderRadius: 5,
 		marginBottom: 10,
 		alignSelf: 'stretch',
 		justifyContent: 'center'
@@ -59,28 +72,23 @@ var styles = StyleSheet.create({
 	},
 	loginTextInputViews: {
 		flexDirection: "row",
-		flex: 1,
 	},
 	loginTextInputMainView: {
-		height: 90,
+		height: 95,
 		borderColor: '#f0f8f5',
 		borderWidth: 3,
-		marginLeft: 50,
-		marginRight: 50,
 		borderWidth: 1,
-		borderRadius: 8,
+		borderRadius: 5,
 		backgroundColor: "#f0f8f5",
-		flex: 1,
 		flexDirection: 'column',
 		alignSelf: 'stretch',
 		bottom: 20,
 	},
 	loginIcons: {
-		top: 5,
-		left: 5,
-		height: 30,
-		width: 30,
-		alignSelf: 'stretch',
+		top: 10,
+		left: 10,
+		height: 20,
+		width: 20,
 		flexDirection: 'row',
 	},
 	topBar: {
@@ -90,11 +98,33 @@ var styles = StyleSheet.create({
 		alignSelf: 'stretch',
 		backgroundColor: '#72b7a3',
 	},
+	bottomBar: {
+		height: 40,
+		flexDirection: 'column',
+		alignSelf: 'stretch',
+		backgroundColor: '#72b7a3',
+	},
 	separationBar: {
 		backgroundColor: "#1d8377",
 		height: 1,
-		width: 260,
-		alignSelf: 'center',
+		marginLeft: 5,
+		marginRight: 5,
+		flexDirection: 'column',
+		alignSelf: 'stretch',
+	},
+	forgotPassword: {
+		textAlign: 'left',
+		color: "gray",
+		alignSelf: "flex-start",
+	},
+	skipSyle: {
+		color: "gray",
+		flexDirection: "row",
+		alignSelf: "center",
+		marginBottom: 10
+	},
+	linkStyle: {
+		color: "blue",
 	}
 });
 
@@ -113,41 +143,66 @@ class LoginScreen extends Component {
 		}
 	}
 
+	signUpPressed() {
+
+	}
+
+	forgotPasswordPressed() {
+
+	}
+
+	skipPressed() {
+
+	}
+
 	render() {
 		return (
 			<View style={styles.container}>
 				<View style={styles.topBar}></View>
-				<Image
-					style={styles.icon}
-					source={require('../../loginLogo.png')}/>
-
-				<Text style={styles.titleStyle}>
-					{"TREASURE HUNT"}
-				</Text>
-
-				<View style={styles.loginTextInputMainView}>
-					<View style={styles.loginTextInputViews}><Image
-						style={styles.loginIcons}
-						source={require('../../user.png')}/>
-					<TextInput style= {styles.textField}
-						placeholder="Username"/>
-					</View>
-					<View style={styles.separationBar}></View>
-					<View style={styles.loginTextInputViews}>
+				
+				<View style={styles.innerContainer}>
 					<Image
-						style={styles.loginIcons}
-						source={require('../../password.png')}/>
-					<TextInput style={styles.textField}
-						secureTextEntry={true}
-						placeholder="Password"/>
-					</View>
-				</View>
+						style={styles.icon}
+						source={require('../../loginLogo.png')}/>
 
-				<TouchableHighlight style={styles.button}
-					onPress={this.onLoginPressed.bind(this)}
-					underlayColor='#99d9f4'>
-		    		<Text style={styles.buttonText}>Sign in</Text>
-		  		</TouchableHighlight>
+					<Text style={styles.titleStyle}>
+						{"TREASURE HUNT"}
+					</Text>
+
+					<View style={styles.loginTextInputMainView}>
+						<View style={styles.loginTextInputViews}><Image
+							style={styles.loginIcons}
+							source={require('../../user.png')}/>
+						<TextInput style= {styles.textField}
+							placeholder="Username"/>
+						</View>
+						<View style={styles.separationBar}></View>
+						<View style={styles.loginTextInputViews}>
+						<Image
+							style={styles.loginIcons}
+							source={require('../../password.png')}/>
+						<TextInput style={styles.textField}
+							secureTextEntry={true}
+							placeholder="Password"/>
+						</View>
+					</View>
+
+					<TouchableHighlight style={styles.button}
+						onPress={this.onLoginPressed.bind(this)}
+						underlayColor='#cadb66'>
+			    		<Text style={styles.buttonText}>Sign in</Text>
+			  		</TouchableHighlight>
+
+			  		<Text style={styles.forgotPassword} onPress={this.forgotPasswordPressed.bind(this)}>Forgot your password?</Text>
+			  		<Text style={styles.forgotPassword}>
+			  			New? Sign up <Text style={styles.linkStyle} onPress={this.signUpPressed.bind(this)}>
+			  				here
+		  				</Text>
+	  				</Text>
+
+				</View>
+		  		<Text style={styles.skipSyle} onPress={this.skipPressed.bind(this)}>No thanks, just take me to the puzzle</Text>
+		  		<View style={styles.bottomBar}></View>
 		  	</View>
 	  	);
 	}
