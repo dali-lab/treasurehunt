ccf0e27b0da7e1bb247e40aed3a76a51a3eb6b92
johnlev <john.lyme@mac.com>
2016-05-17 00:35:04 -0400

    First try with updating login screen

--- src/components/LoginScreen.js
+++ src/components/LoginScreen.js
@@ -14,27 +14,34 @@ var {
 } = React;
 
 var styles = StyleSheet.create({
-	description: {
-		marginBottom: 40,
-		width: 90,
-		height: 90,
+	icon: {
+		marginTop: 50,
+		width: 144,
+		height: 120,
 	},
 	container: {
-		padding: 30,
-		marginTop: 50,
 		alignItems: 'center'
+	},
+	titleStyle: {
+		marginTop: 20,
+		marginBottom: 40,
+		fontSize: 25,
+		color: "#59aa91",
 	},
 	buttonText: {
 		fontSize: 18,
 		color: 'white',
-		alignSelf: 'center'
+		alignSelf: 'center',
+		fontWeight: 'bold',
 	},
 	button: {
 		height: 36,
 		flex: 1,
+		marginLeft: 50,
+		marginRight: 50,
 		flexDirection: 'row',
-		backgroundColor: '#48BBEC',
-		borderColor: '#48BBEC',
+		backgroundColor: '#bfcf60',
+		borderColor: '#bfcf60',
 		borderWidth: 1,
 		borderRadius: 8,
 		marginBottom: 10,
@@ -44,17 +51,27 @@ var styles = StyleSheet.create({
 	textField: {
 		height: 40,
 		flex: 1,
+		marginBottom: 10,
 		flexDirection: 'row',
 		fontSize: 20,
 		paddingLeft: 20,
 		paddingRight: 20,
 	},
-	loginTextInputStyle: {
-		height: 45,
-		borderColor: 'gray',
-		borderWidth: 3,
+	loginTextInputViews: {
+		flexDirection: "row",
 		flex: 1,
-		flexDirection: 'row',
+	},
+	loginTextInputMainView: {
+		height: 90,
+		borderColor: '#f0f8f5',
+		borderWidth: 3,
+		marginLeft: 50,
+		marginRight: 50,
+		borderWidth: 1,
+		borderRadius: 8,
+		backgroundColor: "#f0f8f5",
+		flex: 1,
+		flexDirection: 'column',
 		alignSelf: 'stretch',
 		bottom: 20,
 	},
@@ -66,6 +83,19 @@ var styles = StyleSheet.create({
 		alignSelf: 'stretch',
 		flexDirection: 'row',
 	},
+	topBar: {
+		height: 40,
+		marginLeft: 0,
+		flexDirection: 'row',
+		alignSelf: 'stretch',
+		backgroundColor: '#72b7a3',
+	},
+	separationBar: {
+		backgroundColor: "#1d8377",
+		height: 1,
+		width: 260,
+		alignSelf: 'center',
+	}
 });
 
 class LoginScreen extends Component {
@@ -86,30 +116,37 @@ class LoginScreen extends Component {
 	render() {
 		return (
 			<View style={styles.container}>
+				<View style={styles.topBar}></View>
 				<Image
-					style={styles.description}
+					style={styles.icon}
 					source={require('../../loginLogo.png')}/>
 
-				<View style={styles.loginTextInputStyle}>
-					<Image
+				<Text style={styles.titleStyle}>
+					{"TREASURE HUNT"}
+				</Text>
+
+				<View style={styles.loginTextInputMainView}>
+					<View style={styles.loginTextInputViews}><Image
 						style={styles.loginIcons}
 						source={require('../../user.png')}/>
 					<TextInput style= {styles.textField}
 						placeholder="Username"/>
-				</View>
-				<View style={styles.loginTextInputStyle}>
+					</View>
+					<View style={styles.separationBar}></View>
+					<View style={styles.loginTextInputViews}>
 					<Image
 						style={styles.loginIcons}
 						source={require('../../password.png')}/>
 					<TextInput style={styles.textField}
 						secureTextEntry={true}
 						placeholder="Password"/>
+					</View>
 				</View>
 
 				<TouchableHighlight style={styles.button}
 					onPress={this.onLoginPressed.bind(this)}
 					underlayColor='#99d9f4'>
-		    		<Text style={styles.buttonText}>Login</Text>
+		    		<Text style={styles.buttonText}>Sign in</Text>
 		  		</TouchableHighlight>
 		  	</View>
 	  	);
