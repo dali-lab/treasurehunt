0a074cbb4c25000d301e035d7aaa100f64a8ebc5
John Kotz <john.lyme@mac.com>
2016-08-14 19:21:03 -0400

    Updated hunt overview page

--- src/components/HuntOverview.js
+++ src/components/HuntOverview.js
@@ -9,14 +9,18 @@ var {
 	View,
 	Text,
 	Component,
+	Dimensions,
+  	AlertIOS,
 	TouchableHighlight
 } = React;
 
+var screenWidth = Dimensions.get('window').width;
+
 var styles = StyleSheet.create({
 	container: {
-		marginTop: 65,
-		paddingRight:30,
-		paddingLeft: 30,
+		marginTop: 70,
+		marginBottom: 50,
+		alignItems: 'center',
 		flex: 1,
 	},
 	separatorSmall: {
@@ -26,45 +30,68 @@ var styles = StyleSheet.create({
 		height: 26,
 	},
 	image: {
-		width: 300,
-		height: 200,
-		alignSelf: 'center'
+		height: 180,
+		width: screenWidth - 25,
+		marginBottom: 10,
+		resizeMode: "contain",
 	},
 	description: {
-	    paddingTop: 3,
-	    paddingBottom: 8,
-	    paddingRight: 23,
-	    paddingLeft: 23,
-	    alignSelf: 'center',
+		marginLeft: 30,
+		marginRight: 30,
 		fontSize: 16,
+		alignSelf: "flex-start",
 		fontFamily: 'Verlag-Book',
+		textAlign: "left",
 		color: '#242021',
     },
 	title: {
-		fontSize: 25,
-		margin: 5,
-		color: '#242021',
-		alignSelf: 'center',
+		fontSize: 30,
+		marginTop: 15,
+		marginBottom: 10,
+		letterSpacing: 2,
+		color: '#505050',
 		fontFamily: 'Verlag-Book',
 	},
 	buttonText: {
-	  fontSize: 18,
-	  color: 'white',
-	  alignSelf: 'center',
-	  fontWeight: 'bold',
+		fontSize: 18,
+		color: 'white',
+		fontWeight: 'bold',
 		fontFamily: 'Verlag-Book'
 	},
 	button: {
-	  height: 36,
-	  flexDirection: 'column',
-	  backgroundColor: '#cadb66',
-	  borderColor: '#cadb66',
-	  justifyContent: 'center',
-	  borderWidth: 1,
-	  borderRadius: 8,
-	  marginBottom: 10,
-	  alignSelf: 'stretch',
-	  padding:20,
+		marginLeft: 25,
+		marginRight: 25,
+		height: 36,
+		flexDirection: 'column',
+		backgroundColor: '#cadb66',
+		borderColor: '#cadb66',
+		justifyContent: 'center',
+		alignItems: 'center',
+		borderWidth: 1,
+		borderRadius: 8,
+		marginBottom: 10,
+		alignSelf: 'stretch',
+		padding:20,
+	},
+	actionBarView: {
+		flexDirection: "row",
+		width: screenWidth,
+		marginBottom: 10
+	},
+	likeAction: {
+		marginLeft: 40
+	},
+	shareAction: {
+		flex: 1,
+		alignSelf: "center",
+		marginLeft: 70
+	},
+	rateAction: {
+		marginRight: -10
+	},
+	actionBarIcons: {
+		resizeMode: "contain",
+		height: 20,
 	}
 });
 
@@ -127,20 +154,49 @@ var HuntOverview = React.createClass({
 		}
 	},
 
+	showUnimplemented: function() {
+		AlertIOS.alert(
+			"Not done yet!",
+			"We have yet to implment this feature. Come back soon and try again"
+		);
+	},
+
 	render: function() {
 		var hunt = this.props.hunt;
+
+		var actionBar = <View style={styles.actionBarView}>
+			<TouchableHighlight
+				style={styles.likeAction}
+				onPress={this.showUnimplemented}
+				underlayColor='white'>
+				<Image source={require("./loveIcon.png")} style={styles.actionBarIcons}/>
+			</TouchableHighlight>
+			<TouchableHighlight
+				style={styles.shareAction}
+				onPress={this.showUnimplemented}
+				underlayColor='white'>
+				<Image source={require("./shareIcon.png")} style={styles.actionBarIcons}/>
+			</TouchableHighlight>
+			<TouchableHighlight
+				style={styles.rateAction}
+				onPress={this.showUnimplemented}
+				underlayColor='white'>
+				<Image source={require("./ratingIcon.png")} style={styles.actionBarIcons}/>
+			</TouchableHighlight>
+		</View>
+
 //		console.log(hunt.category);
 		this.updateShouldShowAddButton();
 		return (
 			<View style={styles.container}>
 				<View>
-					<Text style={styles.title}>{hunt.title.toUpperCase()}</Text>
+					<Text style={styles.title}>{hunt.title}</Text>
 				</View>
 				<Image style={styles.image}
 					source={{uri: hunt.image}} />
-				<View style={styles.separatorSmall}/>
+				{actionBar}
 				<Text style={styles.description}>{hunt.description}</Text>
-				<View style={styles.separatorLarge}/>
+				<View style={{flex: 1}}/>
 				<TouchableHighlight style = {styles.button}
 						onPress={this.onStartPressed}
 						underlayColor='#99d9f4'>
@@ -150,11 +206,6 @@ var HuntOverview = React.createClass({
 						onPress={this.onAddHuntPressed}
 						underlayColor='#99d9f4'>
 						<Text style = {styles.buttonText}>{ this.state.shouldShowAddButton ? "ADD HUNT" : "REMOVE HUNT" }</Text>
-				</TouchableHighlight>
-				<TouchableHighlight style = {styles.button}
-						onPress={this.onExitPressed}
-						underlayColor='#99d9f4'>
-						<Text style = {styles.buttonText}>RETURN HOME</Text>
 				</TouchableHighlight>
 			</View>
 		);
