7e8ca629da0f28518663b94530a2639ca3b07167
John Kotz <john.lyme@mac.com>
2016-09-09 03:58:15 -0400

    Sending out new version

--- src/components/RewardModal.js
+++ src/components/RewardModal.js
@@ -26,36 +26,46 @@ var styles = StyleSheet.create({
 	    justifyContent: 'center',
 	    flexDirection: "row",
 	    backgroundColor: "rgba(0, 0, 0, 0.5)",
-	    padding: 20,
+	    padding: 30,
 	},
 	innerContainer: {
 		flex: 1,
 	    borderRadius: 10,
 	    marginTop: 7,
 	    marginBottom: 7,
+	    marginRight: 20,
+	    marginLeft: 20,
 	    padding: 10,
 	    alignItems: 'center',
 	    backgroundColor: "#fff8bd"
 	},
 	doneButton: {
-		backgroundColor: "#ffe37e",
+		backgroundColor: "#f4cd44",
 		width: 200,
 		borderRadius: 10,
-		padding: 10
+		padding: 10,
+		marginBottom: 10
 	},
 	doneButtonText: {
 		color: "black",
 		alignSelf: "center",
+		justifyContent: "center",
 		fontSize: 17,
-		marginBottom: 10
 	},
 	rewardText: {
 
 		textAlign: "center",
-		marginRight: 20,
+		marginRight: 10,
 		fontSize: 15,
-		marginLeft: 20,
-		marginBottom: 10
+		marginLeft: 10,
+		marginBottom: 20
+	},
+	iconImage: {
+		width: 130,
+		height: 130,
+		resizeMode: "contain",
+		marginTop: 10,
+		marginBottom: 20
 	}
 });
 
@@ -67,6 +77,9 @@ var RewardModal = React.createClass({
 	render: function() {
 		return (<View style={styles.container}>
 			<View style={styles.innerContainer}>
+				<Image
+					style={styles.iconImage}
+					source={require("./rewardHeadPeice.png")}/>
 
 				<Text style={styles.rewardText}>{this.props.hunt.reward}</Text>
 
