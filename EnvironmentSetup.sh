d1ba6a8a024c052afea5db178eb33c6ba813e9a1
John Kotz <john.lyme@mac.com>
2016-09-22 23:52:39 -0400

    Pushit push

---
+++ EnvironmentSetup.sh
@@ -0,0 +1,83 @@
+#/bin/bash!
+# Navigate to the folder in which you want the treasurehunt subfolder stored
+
+echo "Confirm that you have Xcode 7.3.* or 8.* (Latest version)"
+echo "You will need this to run the application in a simulator"
+echo "Press ENTER when you have opened it and installed the tools (only if you have not launched it yet)"
+read newLine
+
+echo Checking brew...
+brew help > /dev/null
+if [[ $? != 0 ]]; then
+    echo Installing Homebrew
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    brew update
+echo Installed
+else
+echo Already installed
+fi
+
+echo Checking npm...
+npm -v &> /dev/null
+
+if [[ $? != 0 ]]; then
+    echo Installing...
+    brew install npm && npm install -g react-native-cli
+    if [[ $? != 0 ]]; then
+        echo ERROR: Failed to install npm!
+        exit $?
+    fi
+    echo Installed
+else
+    echo Already installed. Installing CLI...
+    npm install -g react-native-cli 2> /dev/null
+    echo Installed
+fi
+
+echo Checking Cocoapods...
+pod &> /dev/null
+
+if [[ $? != 0 ]]; then
+    echo Installing...
+    brew install cocoapods
+    echo Installed
+else
+    # In case they have an outdated version
+    brew upgrade cocoapods &> /dev/null
+fi
+
+git clone https://github.com/dali-lab/treasurehunt.git ./treasurehunt && cd ./treasurehunt
+
+if [[ $? != 0 ]]; then
+    echo; echo;
+    echo Failed to clone repository.
+    echo Check to see that the repo exists and there isn\'t already a file/directory named treasurehunt
+    exit $?
+fi
+
+npm install
+cp RCTTabBarItem.m ./node_modules/react-native/React/Views/
+cd ios
+# I'm deintegrating and deleting the workspace because the pod files may be out of date. I just want to be certain they are in sync with the rest of the project
+pod deintegrate &> /dev/null
+rm -rf ./treasurehunt.xcworkspace
+pod install
+cd ..
+
+watchman watch-del-all &> /dev/null
+
+sudo xcode-select --install &> /dev/null
+if [[ $? == 0 ]]; then
+   echo Downloading Xcode command line tools...
+   echo You will be able to use your computer as you would, just let the download finish before shutting down the computer
+fi
+
+echo -e "\n\n-----Complete-------"
+echo -e "The script has finished installing all the required items!"
+echo -e "To run the application type the following"
+echo -e "\tcd treasurehunt"
+echo -e "\treact-native run-ios"
+echo -e "\n\n-- General Info --"
+echo -e "The files with the majority of the code are in"
+echo -e "\t./src/components/"
+echo -e "If you want to make changes to the ios project itself, the workspace is in ./ios/"
--- ios/Pods/Target Support Files/Pods-TreasureHunt/Pods-TreasureHunt-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_treasurehunt : NSObject
-@end
-@implementation PodsDummy_Pods_treasurehunt
-@end
