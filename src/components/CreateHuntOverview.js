add0631995ffd56273241bd868fcb4ca9a448172
EdTech Loaner <edtechloaner@edtech-loaner-28.wireless.dartmouth.edu>
2016-10-11 23:29:09 -0400

    Edited CreateHuntOverview

--- src/components/CreateHuntOverview.js
+++ src/components/CreateHuntOverview.js
@@ -1,10 +1,15 @@
-var ReactNative = require('react-native');
-var React = require('react');
-var PageControl = require('react-native-page-control');
-var ClueCompleteModal = require('./ClueCompleteModal');
-var RewardModal = require('./RewardModal');
-var User = require('./User');
-var dismissKeyboard = require('dismissKeyboard');
+const ReactNative = require('react-native');
+const React = require('react');
+const PageControl = require('react-native-page-control');
+const ClueCompleteModal = require('./ClueCompleteModal');
+const RewardModal = require('./RewardModal');
+const User = require('./User');
+const dismissKeyboard = require('dismissKeyboard');
+const Firebase = require('firebase');
+const config = require('../../config');
+
+import rootRef from '../../newfirebase.js'
+
 
 var {
 	StyleSheet,
@@ -28,119 +33,58 @@ var screenHeight = Dimensions.get('window').height;
 
 
 var styles = StyleSheet.create({
-	container: {
-		marginTop: 65,
-		paddingRight:30,
-		paddingLeft: 30,
-		marginBottom: 60,
-		flex: 1,
-	},
-	huntTitle: {
-		fontSize: 20,
-		margin: 10,
-		color: '#656565',
-		alignSelf: 'center',
-		fontFamily: 'Verlag-Book'
-	},
-    topSeparator: {
-        height: 2,
-        backgroundColor: '#5da990'
-    },
-	separatorSmall: {
-		height: 16,
-	},
-	separator: {
-        height: 1,
-        backgroundColor: '#dddddd'
-    },
-    clueName: {
-        fontSize: 20,
-        color: '#000000',
-        fontStyle: 'italic'
-    },
-    modal: {
-	    height: 300,
-	    width: 300,
-  	},
-  	btn: {
-	    margin: 10,
-	    backgroundColor: "#3B5998",
-	    color: "white",
-	    padding: 10,
-  	},
-
-  	btnModal: {
-	    position: "absolute",
-	    top: 0,
-	    right: 0,
-	    width: 50,
-	    height: 50,
-	    backgroundColor: "transparent",
-  	},
-    description: {
-        paddingTop: 3,
-        paddingBottom: 8,
-    },
-    buttonText: {
-	  fontSize: 18,
-	  color: 'white',
-	  alignSelf: 'center',
-	  fontWeight: 'bold'
-	},
-	button: {
-	  height: 36,
-	  marginTop: 26,
-	  flexDirection: 'column',
-	  backgroundColor: '#5da990',
-	  borderColor: '#5da990',
-	  justifyContent: 'center',
-	  borderWidth: 1,
-	  borderRadius: 8,
-	  marginBottom: 10,
-	  alignSelf: 'stretch',
-	  padding:20,
-	  marginLeft: 5,
-	  marginRight: 5,
-	  paddingTop:20,
-	},
-	hint: {
-		textAlign: 'left',
-		color: "gray",
-		alignSelf: "flex-start",
-	},
-	imageContainer: {
-		flexDirection: "column",
-		flex: 1,
-	},
-	pageControl: {
-		alignSelf: 'center',
-		bottom: 0
-	},
-	scrollView: {
-		flex: 1,
-		marginBottom: 5
-	},
-	skipButton: {
-		marginTop: 5,
-		alignSelf: "flex-end",
-	},
-	skipButtonText: {
-		color: "gray"
-	},
 	heading: {
-		fontSize: 20,
+		marginTop: 90,
+		fontSize: 28,
 		fontFamily: "Verlag-Book",
 		alignSelf: "center",
-		width: 100,
+		width: 180,
 		height: 15,
 		flex: 1
-	}
+	},
+	topViewStyle: {
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'space-between'
+    },
+    middleViewStyle: {
+    	flex: 1,
+    	flexDirection: 'row',
+    	justifyContent: "space-between",
+
+    },
+    addButton: {
+    	width: 100,
+    	height: 100,
+    	backgroundColor: '#22AF8E',
+    	alignSelf: "center",
+    	fontSize: 43,
+    	paddingLeft: 36,
+    	paddingTop: 19,
+    	color: "white",
+    	marginRight: 35,
+    	marginBottom: 935
+    },
+    textBoxHunt: {
+    	width: 200,
+    	height: 100,
+       	backgroundColor:'#E1EEEC',
+       	marginLeft: 30,
+       	padding: 10,
+       	fontSize: 19,
+       	fontFamily: "Verlag-Book",
+    	alignSelf: "center",
+    	marginBottom: 935,
+    	borderRadius: 10
+    },
+	divider: {
+    	width: 320,
+    	height: 2,
+    	backgroundColor: '#23B090',
+    	alignSelf: "center",
+    	marginBottom: 530
+	},
 });
-
-const Firebase = require('firebase')
-const config = require('../../config')
-
-import rootRef from '../../newfirebase.js'
 
 
 const usersRef = rootRef.ref('users');
@@ -154,8 +98,18 @@ const storageRef = storage.ref();
  */
 var CreateHunt = React.createClass({
 		render: function(){
-			<View style={styles.heading}>
-				<Text>HUNT NAME</Text>
-			</View>
+			return (
+				<View style={styles.topViewStyle}>
+					<Text style={styles.heading}>HUNT NAME</Text>
+					<View style={styles.divider}/>
+					<View style={styles.middleViewStyle}>
+						<TextInput style={styles.textBoxHunt} multiline = "true" placeholder="Hunt description..."/>
+						<Text style={styles.addButton}>+</Text>
+					</View>
+				</View>
+			);
 		}
 	})
+
+module.exports = CreateHunt;
+
